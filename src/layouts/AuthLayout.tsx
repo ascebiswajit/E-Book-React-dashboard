312041baea3c38a04d1577ed383ebaa61e2cc4ae
biswajit <ascebiswajit@gmail.com>
2024-06-09 04:29:22 +0530

    add condition not redirect to login

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,10 +3,10 @@ import { Navigate, Outlet } from "react-router-dom"
 
 const AuthLayout = () => {
   const token = useTokenStore((state)=>state.token);
-  // console.log(token)
-  // if(token ){
-  //   return <Navigate to={'/dashboard/home'} replace />
-  // }
+  console.log(token)
+  if(token ){
+    return <Navigate to={'/dashboard/home'} replace />
+  }
   return (
     <>
     <Outlet/>
